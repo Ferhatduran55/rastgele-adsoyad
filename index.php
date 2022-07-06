064b28d2f266169f1fb59201d99fb0f894f092ee
Ferhatduran55 <42141771+Ferhatduran55@users.noreply.github.com>
2022-07-06 15:59:35 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+define('ROOT', dirname(__FILE__).DIRECTORY_SEPARATOR);
+require_once "Classes/Main.php";
+?>
+<!DOCTYPE html>
+<html lang="<?=substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,2);?>">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Rastgeleİsim</title>
+    <?php FileManagement::GetAssets("css"); ?>
+    <?php FileManagement::GetAssets("js"); ?>
+  </head>
+  <body class="bg-dark">
+    <?php
+    $user = new User(UserManagement::GenerateName(),UserManagement::GenerateSurname());
+    ?>
+    <div class="container-fluid">
+      <div class="row">
+        <div style="font-size:4vh;" class="col-12 m-1 p-2 bg-info text-white text-center">
+          <?=$user->get_fullname();?>
+        </div>
+        <div class="col-12 d-flex" style="align-items:center;justify-content:center;">
+          <button onclick="window.location.href='';" class="btn btn-success">Yeni Üret</button>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
