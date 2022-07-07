a5ba85a078e2baebcc354dfcd3410f8897a7a605
Ferhatduran55 <42141771+Ferhatduran55@users.noreply.github.com>
2022-07-07 14:58:12 +0300

    Ajax ile phpden rastgele isim çekme eklendi.

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 define('ROOT', dirname(__FILE__).DIRECTORY_SEPARATOR);
 require_once "Classes/Main.php";
+if(isset($_POST['type'])){
+  $type=$_POST['type'];
+  if($type=="NewUser"){
+    $ajaxName=UserManagement::NewUser();
+    echo $ajaxName->get_fullname();
+    exit;
+  }
+}
 ?>
 <!DOCTYPE html>
 <html lang="<?=substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,2);?>">
@@ -15,11 +23,11 @@ require_once "Classes/Main.php";
     <?php $user = UserManagement::NewUser(); ?>
     <div class="container-fluid">
       <div class="row">
-        <div style="font-size:4vh;" class="col-12 m-1 p-2 bg-info text-white text-center">
+        <div id="name" style="font-size:4vh;" class="col-12 m-1 p-2 bg-info text-white text-center">
           <?=$user->get_fullname();?>
         </div>
         <div class="col-12 d-flex" style="align-items:center;justify-content:center;">
-          <button onclick="window.location.href='';" class="btn btn-success">Yeni Üret</button>
+          <button id="Generate" class="btn btn-success">Yeni Üret</button>
         </div>
       </div>
     </div>
