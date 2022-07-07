b868a3e8fff3b4c0a15d351a8fa2c8a9a46cf1ad
Ferhatduran55 <42141771+Ferhatduran55@users.noreply.github.com>
2022-07-07 12:10:09 +0300

    Otomatik class algılama eklendi.

--- Classes/Main.php
+++ Classes/Main.php
@@ -2,19 +2,24 @@
 class Main
 {
   static $dir = "Classes/";
-  static $classes = array(
-    "FullName",
-    "User",
-    "UserManagement",
-    "FileManagement"
-  );
+  static $classes = array();
   static $prefixClass = "";
-  static $suffixClass = ".class.php";
+  static $suffixClass = "";
   public static function StartClasses()
   {
+    $thisFile = "Main.php";
+    $classDir = scandir("./Classes");
+    foreach($classDir as $classDirFile)
+    {
+      $file = self::$suffixClass.$classDirFile.self::$prefixClass;
+      if($thisFile!=$file && $file!="Files" && $file!="." && $file != "..")
+      {
+        array_push(self::$classes,$file);
+      }
+    }
     foreach(self::$classes as $class)
     {
-      $classFile = self::$dir.self::$prefixClass.$class.self::$suffixClass;
+      $classFile = self::$dir.self::$suffixClass.$class.self::$prefixClass;
       if(file_exists($classFile))
       {
         require_once($classFile);
